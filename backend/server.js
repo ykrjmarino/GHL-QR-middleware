74007a38546da32c4e21ffdd6eef62a59db0ad91
YlaMarino <marinoyla333@gmail.com>
2026-04-01 22:33:17 +0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Backend is running QR proj"));
+
+app.listen(port, () => {
+  console.log(`✅ Backend running at http://localhost:${port} (ykrjm2026)`);
+});
